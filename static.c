c4bbe477c5003d324238ca46c11a56cd64336d3e
anish0204 <47878071+anish0204@users.noreply.github.com>
2019-02-24 17:54:19 +0530

    Create static.c

---
+++ static.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    printf("%d",a[10]);
+    int i=10;
+    int x,y;
+    int *ptr;
+    printf("%d",*ptr);
+    int b;
+    b++;
+}
