d4abfae1ca0a05ff245b634d681ed77b9df5b607
anish0204 <47878071+anish0204@users.noreply.github.com>
2019-02-24 18:47:12 +0530

    Update static.c

--- static.c
+++ static.c
@@ -5,10 +5,11 @@ int main()
     int a[10] = {1,2,3,4,5,6,7,8,9,10};
     printf("%d",a[9]);
     int i=10;
-    int x,y;
+   
     int *ptr;
     //ptr=&i;
     printf("%d",*ptr);
+    printf("%d",i);
     int b;
     b++;
     printf("%d",b);
